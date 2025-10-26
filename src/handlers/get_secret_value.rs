6c2135f91c53195bb046cc8fb10179531ca46113
Jacobtread <jacobtread@gmail.com>
2025-10-26 21:19:40 +1300

    feat: GetSecretValue should error for a secret that is scheduled for deletion

--- src/handlers/get_secret_value.rs
+++ src/handlers/get_secret_value.rs
@@ -1,6 +1,3 @@
-use axum::response::{IntoResponse, Response};
-use serde::{Deserialize, Serialize};
-
 use crate::{
     database::{
         DbPool,
@@ -12,9 +9,14 @@ use crate::{
     },
     handlers::{
         Handler,
-        error::{AwsErrorResponse, ResourceNotFoundException},
+        error::{
+            AwsErrorResponse, InternalServiceError, InvalidRequestException,
+            ResourceNotFoundException,
+        },
     },
 };
+use axum::response::{IntoResponse, Response};
+use serde::{Deserialize, Serialize};
 
 // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
 pub struct GetSecretValueHandler;
@@ -54,26 +56,33 @@ impl Handler for GetSecretValueHandler {
     async fn handle(db: &DbPool, request: Self::Request) -> Result<Self::Response, Response> {
         let secret_id = request.secret_id;
         let version_id = request.version_id;
-        let version_stage = request
+        let version_stage = match request
             .version_stage
             .map(VersionStage::try_from)
             .transpose()
-            .expect("todo: handle unknown version stage");
+        {
+            Ok(value) => value,
+            Err(_error) => {
+                return Err(AwsErrorResponse(InvalidRequestException).into_response());
+            }
+        };
 
         let secret = match (&version_id, version_stage) {
-            (None, None) => get_secret_latest_version(db, &secret_id).await.unwrap(),
+            (None, None) => get_secret_latest_version(db, &secret_id).await,
             (Some(version_id), Some(version_stage)) => {
-                get_secret_by_version_stage_and_id(db, &secret_id, version_id, version_stage)
-                    .await
-                    .unwrap()
+                get_secret_by_version_stage_and_id(db, &secret_id, version_id, version_stage).await
             }
-            (Some(version_id), None) => get_secret_by_version_id(db, &secret_id, version_id)
-                .await
-                .unwrap(),
+            (Some(version_id), None) => get_secret_by_version_id(db, &secret_id, version_id).await,
             (None, Some(version_stage)) => {
-                get_secret_by_version_stage(db, &secret_id, version_stage)
-                    .await
-                    .unwrap()
+                get_secret_by_version_stage(db, &secret_id, version_stage).await
+            }
+        };
+
+        let secret = match secret {
+            Ok(value) => value,
+            Err(error) => {
+                tracing::error!(?error, %secret_id, ?version_id, ?version_stage, "failed to get secret value");
+                return Err(AwsErrorResponse(InternalServiceError).into_response());
             }
         };
 
@@ -82,9 +91,17 @@ impl Handler for GetSecretValueHandler {
             None => return Err(AwsErrorResponse(ResourceNotFoundException).into_response()),
         };
 
-        update_secret_version_last_accessed(db, &secret.arn, &secret.version_id)
-            .await
-            .unwrap();
+        // Secret is scheduled for deletion
+        if secret.scheduled_delete_at.is_some() {
+            return Err(AwsErrorResponse(InvalidRequestException).into_response());
+        }
+
+        if let Err(error) =
+            update_secret_version_last_accessed(db, &secret.arn, &secret.version_id).await
+        {
+            tracing::error!(?error, name = %secret.name, "failed to update secret last accessed");
+            return Err(AwsErrorResponse(InternalServiceError).into_response());
+        }
 
         let created_at = if version_id.is_some() {
             secret.version_created_at
